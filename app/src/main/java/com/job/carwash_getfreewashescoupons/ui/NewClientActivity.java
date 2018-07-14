0b4879e4dfa142c2704742c7029732318abeb7a1
JobGetabu <getabujob@gmail.com>
2018-07-14 16:46:48 +0300

    injected views add customer

--- app/src/main/java/com/job/carwash_getfreewashescoupons/ui/NewClientActivity.java
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/ui/NewClientActivity.java
@@ -1,15 +1,43 @@
 package com.job.carwash_getfreewashescoupons.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.job.carwash_getfreewashescoupons.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class NewClientActivity extends AppCompatActivity {
+
+    @BindView(R.id.client_toolbar)
+    Toolbar clientToolbar;
+    @BindView(R.id.client_firstname)
+    TextInputLayout clientFirstname;
+    @BindView(R.id.client_lastname)
+    TextInputLayout clientLastname;
+    @BindView(R.id.client_phone)
+    TextInputLayout clientPhone;
+    @BindView(R.id.client_vehiclereg)
+    TextInputLayout clientVehiclereg;
+    @BindView(R.id.client_spinner)
+    Spinner clientSpinner;
+    @BindView(R.id.client_add_btn)
+    TextView clientAddBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_client);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.client_add_btn)
+    public void onViewClicked() {
     }
 }
