73d870120b98a97ae7ade7b23019caa20199c32f
JobGetabu <getabujob@gmail.com>
2018-07-14 23:39:04 +0300

    listener for current user

--- app/src/main/java/com/job/carwash_getfreewashescoupons/ui/MainActivity.java
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/ui/MainActivity.java
@@ -2,12 +2,15 @@ package com.job.carwash_getfreewashescoupons.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.job.carwash_getfreewashescoupons.R;
 
 import butterknife.BindView;
@@ -22,14 +25,19 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton fabButton;
 
     private FilterDialogFragment mFilterDialog;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
         setSupportActionBar(mToolbar);
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
+        authListner();
 
         // Filter Dialog
         mFilterDialog = new FilterDialogFragment();
@@ -63,5 +71,36 @@ public class MainActivity extends AppCompatActivity {
     public void onFilterClicked() {
         // Show the dialog containing filter options
         mFilterDialog.show(getSupportFragmentManager(), FilterDialogFragment.TAG);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            sendToLogin();
+        }
+    }
+
+    private void sendToLogin() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+
+    //auth state listener for live changes
+    private void authListner() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // Sign in logic here.
+                    sendToLogin();
+                }
+            }
+        };
     }
 }
