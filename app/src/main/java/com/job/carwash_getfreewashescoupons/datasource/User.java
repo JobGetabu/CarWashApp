010e4b055f552399811cc3ade22b9f2abd2ad61c
JobGetabu <getabujob@gmail.com>
2018-07-14 15:23:07 +0300

    user pojo

---
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/datasource/User.java
@@ -0,0 +1,39 @@
+package com.job.carwash_getfreewashescoupons.datasource;
+
+/**
+ * Created by Job on Saturday : 7/14/2018.
+ */
+public class User {
+
+    private String username;
+    private String devicetoken;
+
+    public User(String username, String devicetoken) {
+        this.username = username;
+        this.devicetoken = devicetoken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDevicetoken() {
+        return devicetoken;
+    }
+
+    public void setDevicetoken(String devicetoken) {
+        this.devicetoken = devicetoken;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", devicetoken='" + devicetoken + '\'' +
+                '}';
+    }
+}
