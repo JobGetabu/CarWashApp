b807e5dc8f894dcad7524a36382635c26295111d
JobGetabu <getabujob@gmail.com>
2018-07-14 13:21:07 +0300

    inflated menu

--- app/src/main/java/com/job/carwash_getfreewashescoupons/ui/MainActivity.java
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/ui/MainActivity.java
@@ -1,7 +1,9 @@
 package com.job.carwash_getfreewashescoupons.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.job.carwash_getfreewashescoupons.R;
 
@@ -11,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main_search:
+                break;
+            case R.id.main_logout:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
