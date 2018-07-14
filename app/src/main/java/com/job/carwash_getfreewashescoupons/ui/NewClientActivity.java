d733654eb197696095be6815f03e03f4610202d2
JobGetabu <getabujob@gmail.com>
2018-07-15 00:32:34 +0300

    logic to save db data

--- app/src/main/java/com/job/carwash_getfreewashescoupons/ui/NewClientActivity.java
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/ui/NewClientActivity.java
@@ -1,7 +1,9 @@
 package com.job.carwash_getfreewashescoupons.ui;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,8 +11,15 @@ import android.util.Log;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.job.carwash_getfreewashescoupons.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,8 +47,10 @@ public class NewClientActivity extends AppCompatActivity {
     TextView clientAddBtn;
 
     private static final String TAG = "NewClient";
+    private static final String CUSTOMERINFOCOL = "CustomerInfo";
     private PhoneNumberUtil mPhoneNumberUtil;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +63,7 @@ public class NewClientActivity extends AppCompatActivity {
 
         //firebase
         mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
 
         mPhoneNumberUtil = PhoneNumberUtil.createInstance(this);
 
@@ -67,7 +79,7 @@ public class NewClientActivity extends AppCompatActivity {
 
     private void saveToDb(){
         final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
-        pDialog.getProgressHelper().setBarColor(Color.parseColor("#f9ab60"));
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#e61cb2d7"));
         pDialog.setTitleText("Saving Changes...");
         pDialog.setCancelable(false);
         pDialog.show();
@@ -78,6 +90,48 @@ public class NewClientActivity extends AppCompatActivity {
         String vehReg = clientVehiclereg.getEditText().getText().toString();
         String vehType = clientSpinner.getSelectedItem().toString();
 
+        String key = mFirestore.collection(CUSTOMERINFOCOL).document().getId();
+
+        Map<String,Object> clientMap= new HashMap<>();
+        clientMap.put("customerId",key);
+        clientMap.put("firstname",firstname);
+        clientMap.put("lastname",lname);
+        clientMap.put("phonenumber",phone);
+        clientMap.put("vehiclereg",vehReg);
+        clientMap.put("vehicletype",vehType);
+        clientMap.put("ownerid",mAuth.getCurrentUser().getUid());
+        clientMap.put("regdate", FieldValue.serverTimestamp());
+
+        mFirestore.collection(CUSTOMERINFOCOL).document(key)
+                .set(clientMap)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> dbtask) {
+                        if (dbtask.isSuccessful()){
+                            pDialog.changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                            pDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sDialog) {
+                                    sDialog.dismissWithAnimation();
+                                    startActivity(new Intent(NewClientActivity.this,MainActivity.class));
+                                }
+                            });
+
+                            finish();
+                        }else {
+                            pDialog.dismiss();
+                            Log.d(TAG, "onComplete: error" + dbtask.getException().toString());
+                            errorPrompt();
+                        }
+                    }
+                });
+    }
+
+    private void errorPrompt() {
+        new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                .setTitleText("Oops...")
+                .setContentText("Something went wrong!")
+                .show();
     }
 
     public boolean validate() {
