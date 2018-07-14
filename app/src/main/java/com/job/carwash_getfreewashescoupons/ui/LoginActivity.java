f54f4495b27b9da0626bb6d4e137e23520f81f47
JobGetabu <getabujob@gmail.com>
2018-07-14 23:33:57 +0300

    coded the login google page

--- app/src/main/java/com/job/carwash_getfreewashescoupons/ui/LoginActivity.java
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/ui/LoginActivity.java
@@ -1,15 +1,226 @@
 package com.job.carwash_getfreewashescoupons.ui;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.job.carwash_getfreewashescoupons.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class LoginActivity extends AppCompatActivity {
+
+
+    private static final String USERSCOL = "Users";
+    @BindView(R.id.login_google)
+    Button mBtnGoogleLogin;
+
+    public static final int RC_SIGN_IN = 1001;
+    public static final String TAG = "GoogleLogInFragment";
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    @OnClick(R.id.login_google)
+    public void loginWithGoogleClick() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        mBtnGoogleLogin.setEnabled(false);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void sendToMain() {
+
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    private void errorPrompt() {
+        new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                .setTitleText("Oops...")
+                .setContentText("Something went wrong!")
+                .show();
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#e61cb2d7"));
+        pDialog.setTitleText("Logging in...");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            final FirebaseUser user = mAuth.getCurrentUser();
+
+                            final String device_token = FirebaseInstanceId.getInstance().getToken();
+                            final String mCurrentUserid = mAuth.getCurrentUser().getUid();
+
+                            // refactor this not to write to DB each time...check if account exists
+
+                            DocumentReference docReference = mFirestore.collection("Users").document(mCurrentUserid);
+                            docReference.get()
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                DocumentSnapshot document = task.getResult();
+                                                if (document.exists()) {
+                                                    Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+                                                    //update token only
+                                                    updateTokenOnly(mCurrentUserid, device_token, pDialog);
+
+                                                } else {
+                                                    Log.d(TAG, "No such document");
+
+                                                    //logging in with no pre account
+                                                    //region create fresh account
+
+                                                    pDialog.changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                                                    pDialog.setTitleText("Account doesn't exists! \n Creating one...");
+
+                                                    //write to db
+                                                    writingToUsers(pDialog, device_token, user, mCurrentUserid);
+
+                                                    //endregion
+                                                }
+                                            } else {
+                                                Log.d(TAG, "get failed with ", task.getException());
+                                            }
+                                        }
+                                    });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            pDialog.dismissWithAnimation();
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(android.R.id.content), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            mBtnGoogleLogin.setEnabled(true);
+                        }
+                    }
+                });
+    }
+
+    //possibly first time log in
+    private void writingToUsers(final SweetAlertDialog pDialog, String device_token, FirebaseUser user, String mCurrentUserid) {
+
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("devicetoken", device_token);
+        userMap.put("username", user.getDisplayName());
+
+        DocumentReference usersRef = mFirestore.collection(USERSCOL).document(mCurrentUserid);
+
+
+        usersRef.set(userMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> dbtask) {
+                        if (dbtask.isSuccessful()) {
+                            pDialog.dismissWithAnimation();
+                            sendToMain();
+                        } else {
+                            pDialog.dismiss();
+                            errorPrompt();
+                            Log.d(TAG, "onComplete: error " + dbtask.getException());
+                        }
+                    }
+                });
+    }
+
+    private void updateTokenOnly(final String mCurrentUserid,
+                                 final String device_token,
+                                 final SweetAlertDialog pDialog) {
+
+        mFirestore.collection("Users").document(mCurrentUserid).update("devicetoken", device_token)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> dbtask) {
+                        if (dbtask.isSuccessful()) {
+                            pDialog.dismissWithAnimation();
+                            sendToMain();
+                        } else {
+                            pDialog.dismiss();
+                            errorPrompt();
+                            Log.d(TAG, "onComplete: error " + dbtask.getException());
+                        }
+                    }
+                });
     }
 }
