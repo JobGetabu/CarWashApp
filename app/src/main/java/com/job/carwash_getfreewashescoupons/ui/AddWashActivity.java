52ef924039446d4391f4b22e46fab1ef45d760ac
JobGetabu <getabujob@gmail.com>
2018-07-14 16:45:57 +0300

    designed add client

---
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/ui/AddWashActivity.java
@@ -0,0 +1,15 @@
+package com.job.carwash_getfreewashescoupons.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.job.carwash_getfreewashescoupons.R;
+
+public class AddWashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_wash);
+    }
+}
--- app/src/main/java/com/job/carwash_getfreewashescoupons/ui/MainActivity.java
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.job.carwash_getfreewashescoupons.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,11 +12,14 @@ import com.job.carwash_getfreewashescoupons.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
+    @BindView(R.id.fab_button)
+    FloatingActionButton fabButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +46,11 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.fab_button)
+    public void onViewClicked() {
+        Intent intent = new Intent(this,NewClientActivity.class);
+        startActivity(intent);
     }
 }
