cc1cf1c58aadeb1e0871ff21965e70972bbd3d1f
JobGetabu <getabujob@gmail.com>
2018-07-14 15:46:57 +0300

    added activity add client

---
+++ app/src/main/java/com/job/carwash_getfreewashescoupons/ui/NewClientActivity.java
@@ -0,0 +1,15 @@
+package com.job.carwash_getfreewashescoupons.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.job.carwash_getfreewashescoupons.R;
+
+public class NewClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_client);
+    }
+}
